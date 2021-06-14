051e52b1440a78c42f6939d2eb3d28a8f5fd735a
mustafaguldag <guldag.mustafaa@gmail.com>
2021-06-14 15:32:45 +0300

    commit tree 6e3373773fce52519426fc31e543d3027920ff9f

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,8 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map(props=>{
+        return <span key={props}>{props}</span>})}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,8 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map(pro =>{return <ProjectItem key={pro.id} name={pro.name} about={pro.about} technologies={pro.technologies} />} )}</div>
+      
     </div>
   );
 }
